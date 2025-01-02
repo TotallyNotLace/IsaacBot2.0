0e2e51b06415aa21cd35dec9443c5a4023ea57aa
Joe Ewert <joseph.ewert@gmail.com>
2025-01-02 16:28:32 -0600

    added new icon

--- IsaacBot/IsaacBot/Form1.Designer.cs
+++ IsaacBot/IsaacBot/Form1.Designer.cs
@@ -35,18 +35,18 @@
             this.consoleTextBox = new System.Windows.Forms.TextBox();
             this.consoleLabel = new System.Windows.Forms.Label();
             this.grpSettings = new System.Windows.Forms.GroupBox();
-            this.chkMultiplier = new System.Windows.Forms.CheckBox();
-            this.chkSpamControl = new System.Windows.Forms.CheckBox();
             this.chkMultiCommand = new System.Windows.Forms.CheckBox();
+            this.chkSpamControl = new System.Windows.Forms.CheckBox();
+            this.chkMultiplier = new System.Windows.Forms.CheckBox();
             this.grpTwitch = new System.Windows.Forms.GroupBox();
+            this.lblAuthCode = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblUserName = new System.Windows.Forms.Label();
             this.btnStart = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.btnShowHide = new System.Windows.Forms.Button();
             this.txtChannel = new System.Windows.Forms.TextBox();
             this.lblChannel = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.lblUserName = new System.Windows.Forms.Label();
-            this.lblAuthCode = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.grpSettings.SuspendLayout();
             this.grpTwitch.SuspendLayout();
@@ -118,15 +118,15 @@
             this.grpSettings.TabStop = false;
             this.grpSettings.Text = "Settings";
             // 
-            // chkMultiplier
+            // chkMultiCommand
             // 
-            this.chkMultiplier.AutoSize = true;
-            this.chkMultiplier.Location = new System.Drawing.Point(9, 25);
-            this.chkMultiplier.Name = "chkMultiplier";
-            this.chkMultiplier.Size = new System.Drawing.Size(136, 21);
-            this.chkMultiplier.TabIndex = 0;
-            this.chkMultiplier.Text = "Multiplier";
-            this.chkMultiplier.UseVisualStyleBackColor = true;
+            this.chkMultiCommand.AutoSize = true;
+            this.chkMultiCommand.Location = new System.Drawing.Point(9, 79);
+            this.chkMultiCommand.Name = "chkMultiCommand";
+            this.chkMultiCommand.Size = new System.Drawing.Size(174, 21);
+            this.chkMultiCommand.TabIndex = 2;
+            this.chkMultiCommand.Text = "Multi-Command";
+            this.chkMultiCommand.UseVisualStyleBackColor = true;
             // 
             // chkSpamControl
             // 
@@ -138,15 +138,15 @@
             this.chkSpamControl.Text = "Spam Control";
             this.chkSpamControl.UseVisualStyleBackColor = true;
             // 
-            // chkMultiCommand
+            // chkMultiplier
             // 
-            this.chkMultiCommand.AutoSize = true;
-            this.chkMultiCommand.Location = new System.Drawing.Point(9, 79);
-            this.chkMultiCommand.Name = "chkMultiCommand";
-            this.chkMultiCommand.Size = new System.Drawing.Size(174, 21);
-            this.chkMultiCommand.TabIndex = 2;
-            this.chkMultiCommand.Text = "Multi-Command";
-            this.chkMultiCommand.UseVisualStyleBackColor = true;
+            this.chkMultiplier.AutoSize = true;
+            this.chkMultiplier.Location = new System.Drawing.Point(9, 25);
+            this.chkMultiplier.Name = "chkMultiplier";
+            this.chkMultiplier.Size = new System.Drawing.Size(136, 21);
+            this.chkMultiplier.TabIndex = 0;
+            this.chkMultiplier.Text = "Multiplier";
+            this.chkMultiplier.UseVisualStyleBackColor = true;
             // 
             // grpTwitch
             // 
@@ -165,6 +165,32 @@
             this.grpTwitch.TabStop = false;
             this.grpTwitch.Text = "Twitch Connector";
             // 
+            // lblAuthCode
+            // 
+            this.lblAuthCode.AutoSize = true;
+            this.lblAuthCode.Location = new System.Drawing.Point(11, 131);
+            this.lblAuthCode.Name = "lblAuthCode";
+            this.lblAuthCode.Size = new System.Drawing.Size(102, 17);
+            this.lblAuthCode.TabIndex = 13;
+            this.lblAuthCode.Text = "Auth Code";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(122, 97);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.PasswordChar = '*';
+            this.textBox1.Size = new System.Drawing.Size(209, 25);
+            this.textBox1.TabIndex = 10;
+            // 
+            // lblUserName
+            // 
+            this.lblUserName.AutoSize = true;
+            this.lblUserName.Location = new System.Drawing.Point(11, 100);
+            this.lblUserName.Name = "lblUserName";
+            this.lblUserName.Size = new System.Drawing.Size(105, 17);
+            this.lblUserName.TabIndex = 12;
+            this.lblUserName.Text = "User Name";
+            // 
             // btnStart
             // 
             this.btnStart.Location = new System.Drawing.Point(14, 30);
@@ -173,6 +199,14 @@
             this.btnStart.TabIndex = 6;
             this.btnStart.Text = "Connect";
             this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(122, 128);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
+            this.textBox2.Size = new System.Drawing.Size(209, 25);
+            this.textBox2.TabIndex = 11;
             // 
             // btnShowHide
             // 
@@ -200,40 +234,6 @@
             this.lblChannel.TabIndex = 9;
             this.lblChannel.Text = "Channel";
             // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(122, 97);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.PasswordChar = '*';
-            this.textBox1.Size = new System.Drawing.Size(209, 25);
-            this.textBox1.TabIndex = 10;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(122, 128);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.PasswordChar = '*';
-            this.textBox2.Size = new System.Drawing.Size(209, 25);
-            this.textBox2.TabIndex = 11;
-            // 
-            // lblUserName
-            // 
-            this.lblUserName.AutoSize = true;
-            this.lblUserName.Location = new System.Drawing.Point(11, 100);
-            this.lblUserName.Name = "lblUserName";
-            this.lblUserName.Size = new System.Drawing.Size(105, 17);
-            this.lblUserName.TabIndex = 12;
-            this.lblUserName.Text = "User Name";
-            // 
-            // lblAuthCode
-            // 
-            this.lblAuthCode.AutoSize = true;
-            this.lblAuthCode.Location = new System.Drawing.Point(11, 131);
-            this.lblAuthCode.Name = "lblAuthCode";
-            this.lblAuthCode.Size = new System.Drawing.Size(102, 17);
-            this.lblAuthCode.TabIndex = 13;
-            this.lblAuthCode.Text = "Auth Code";
-            // 
             // mainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 17F);
@@ -248,6 +248,7 @@
             this.Controls.Add(this.consoleTextBox);
             this.Controls.Add(this.groupBox1);
             this.Font = new System.Drawing.Font("Upheaval TT (BRK)", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(6, 4, 6, 4);
             this.Name = "mainForm";
             this.Text = "Isaac Bot 2.0";
