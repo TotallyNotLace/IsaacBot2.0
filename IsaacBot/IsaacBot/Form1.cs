dd57216484b6d2e431eb27cad37c0cc6748fc45b
Joe Ewert <joseph.ewert@gmail.com>
2025-01-02 16:15:29 -0600

    added controls, twitchlib, and graphics

--- IsaacBot/IsaacBot/Form1.cs
+++ IsaacBot/IsaacBot/Form1.cs
@@ -10,11 +10,36 @@ using System.Windows.Forms;
 
 namespace IsaacBot
 {
-    public partial class Form1 : Form
+    public partial class mainForm : Form
     {
-        public Form1()
+        private float numbers = 0;
+
+        public mainForm()
         {
             InitializeComponent();
+        }
+
+
+        private void mainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sendMessageTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sendButton_Click(object sender, EventArgs e)
+        {
+            sendMessageTextBox.Text = "working";
+            consoleTextBox.AppendText($"working {numbers} \r\n");
+            numbers++;
         }
     }
 }
